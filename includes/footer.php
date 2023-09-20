dd0dbb2cdb0bed9df0488c56ed521ecdec751553
juele <juele@172.16.3.131>
2023-09-20 15:11:39 +0200

    Updated the footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer><div class="footer">
-    <p>Emil Juel Korsholms store website, 2023, Version 0.7c.</p>
+    <p>Emil215ps store website, 2023, Version 0.7d.</p>
 </div></footer>
 </body>
 </html>
