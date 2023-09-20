21a6c11d9b0e3f4cc3c79d42e54692efd3a33c68
juele <juele@172.16.3.131>
2023-09-20 14:34:11 +0200

    Commit designed to test changes made to the Github. 2nd test.

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 <footer><div class="footer">
-    <p>Emil Juel Korsholms store website, 2023, Version 0.7b.</p>
+    <p>Emil Juel Korsholms store website, 2023, Version 0.7c.</p>
 </div></footer>
 </body>
 </html>
