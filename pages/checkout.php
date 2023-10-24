cc5c26653a10c86ef8c6ee11003d60f2e19f4f4c
juele <juele@10.130.16.73>
2023-10-24 14:22:21 +0200

    Forms now give feedback and shows when you are in the wrong.

--- pages/checkout.php
+++ pages/checkout.php
@@ -1,31 +1,31 @@
 <form id="checkoutform" action="code/insert.php" method="post">
     <div id="inputform">
         <label><h1>Create user:</h1><br>Name:</label><br>
-    <input class='input-width' type="text" name="name" required><br>
+    <input class='input-width' type="text" name="name" pattern="{1,}" title="A name is required." required><br>
     </div>
     <div id="inputform">
     <label>Address:</label><br>
-    <input class='input-width' type="text" name="address" pattern="[a-zA-ZæøåÆØÅ0-9]" required><br>
+    <input class='input-width' type="text" name="address" pattern="[a-zA-ZæøåÆØÅ0-9 ]{1,}" title="Please enter a valid address." required><br>
     </div>
     <div id="inputform">
     <label>Zip:</label><br>
-    <input class='input-width' type="text" name="zip" pattern="[0-9]" required><br>
+    <input class='input-width' type="text" name="zip" pattern="[0-9]{1,}" title="Please enter a valid zip code." required><br>
     </div>
     <div id="inputform">
     <label>City:</label><br>
-    <input class='input-width' type="text" name="city" pattern="[a-zA-ZæøåÆØÅ]" required><br>
+    <input class='input-width' type="text" name="city" pattern="[a-zA-ZæøåÆØÅ ]{1,}" title="Please enter a valid city." required><br>
     </div>
     <div id="inputform">
     <label>Phone:</label><br>
-    <input class='input-width' type="text" name="phone" pattern="[0-9+]{1,11}" required><br>
+    <input class='input-width' type="text" name="phone" pattern="[0-9+]{1,11}" title="Please enter a valid phone number, formats '12345678' and '+xx12345689' are allowed."  required><br>
     </div>
     <div id="inputform">
     <label>Email:</label><br>
-    <input class='input-width' type="text" name="email" pattern="[a-zA-ZæøåÆØÅ0-9._%+\-]+@[a-zA-ZæøåÆØÅ0-9.\-]+\.[a-zA-ZæøåÆØÅ]{2,}$" required><br>
+    <input class='input-width' type="text" name="email" pattern="[a-zA-ZæøåÆØÅ0-9._%+\-]+@[a-zA-ZæøåÆØÅ0-9.\-]+\.[a-zA-ZæøåÆØÅ]{2,}$" title="Please enter a valid email address." required><br>
     </div>
     <div id="inputform">
     <label>Password:</label><br>
-    <input class='input-width' type="password" name="password" pattern="{8,}" required><br>
+    <input class='input-width' type="password" name="password" minlength="8" title="Please enter a valid password, 8 characters are required" required><br>
     </div>
     <input type="submit" class="btn btn-primary" name="submit" value="Register">
 </form>
@@ -33,11 +33,11 @@
 <form id="checkoutformlogin" action="code/loginbuy.php" method="post">
     <div id="inputformlogin">
     <label><h1>Login:</h1><br>Email:</label><br>
-    <input class='input-width' type="text" name="email" pattern="[a-zA-ZæøåÆØÅ0-9._%+\-]+@[a-zA-ZæøåÆØÅ0-9.\-]+\.[a-zA-ZæøåÆØÅ]{2,}$" required><br>
+    <input class='input-width' type="text" name="email" pattern="[a-zA-ZæøåÆØÅ0-9._%+\-]+@[a-zA-ZæøåÆØÅ0-9.\-]+\.[a-zA-ZæøåÆØÅ]{2,}$" title="Please enter a valid email address." required><br>
     </div>
     <div id="inputformlogin">
     <label>Password:</label><br>
-    <input class='input-width' type="password" name="password" required><br>
+    <input class='input-width' type="password" name="password" title="Enter your password." required><br>
     </div>
     <input type="submit" class="btn btn-primary" name="login" value="Login">
 </form>
--- pages/loginornew.php
+++ pages/loginornew.php
@@ -2,31 +2,31 @@
 <form id="checkoutform" action="code/register.php" method="post">
     <div id="inputform">
         <label><h1>Create user:</h1><br>Name:</label><br>
-    <input class='input-width' type="text" name="name" required><br>
+    <input class='input-width' type="text" name="name" pattern="{1,}" title="A name is required." required><br>
     </div>
     <div id="inputform">
     <label>Address:</label><br>
-    <input class='input-width' type="text" name="address" pattern="[a-zA-ZæøåÆØÅ0-9]" required><br>
+    <input class='input-width' type="text" name="address" pattern="[a-zA-ZæøåÆØÅ0-9 ]{1,}" title="Please enter a valid address." required><br>
     </div>
     <div id="inputform">
     <label>Zip:</label><br>
-    <input class='input-width' type="text" name="zip" pattern="[0-9]" required><br>
+    <input class='input-width' type="text" name="zip" pattern="[0-9]{1,}" title="Please enter a valid zip code." required><br>
     </div>
     <div id="inputform">
     <label>City:</label><br>
-    <input class='input-width' type="text" name="city" pattern="[a-zA-ZæøåÆØÅ]" required><br>
+    <input class='input-width' type="text" name="city" pattern="[a-zA-ZæøåÆØÅ ]{1,}" title="Please enter a valid city." required><br>
     </div>
     <div id="inputform">
     <label>Phone:</label><br>
-    <input class='input-width' type="text" name="phone" pattern="[0-9+]{1,11}" required><br>
+    <input class='input-width' type="text" name="phone" pattern="[0-9+]{1,11}" title="Please enter a valid phone number, formats '12345678' and '+xx12345689' are allowed."  required><br>
     </div>
     <div id="inputform">
     <label>Email:</label><br>
-    <input class='input-width' type="text" name="email" pattern="[a-zA-ZæøåÆØÅ0-9._%+\-]+@[a-zA-ZæøåÆØÅ0-9.\-]+\.[a-zA-ZæøåÆØÅ]{2,}$" required><br>
+    <input class='input-width' type="text" name="email" pattern="[a-zA-ZæøåÆØÅ0-9._%+\-]+@[a-zA-ZæøåÆØÅ0-9.\-]+\.[a-zA-ZæøåÆØÅ]{2,}$" title="Please enter a valid email address." required><br>
     </div>
     <div id="inputform">
     <label>Password:</label><br>
-    <input class='input-width' type="password" name="password" pattern="{8,}" required><br>
+    <input class='input-width' type="password" name="password" minlength="8" title="Please enter a valid password, 8 characters are required" required><br>
     </div>
     <input type="submit" class="btn btn-primary" name="submit" value="Register">
 </form>
