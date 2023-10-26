8379ea23825d65c777532eff2f214e8b5b50ec26
juele <juele@10.130.16.73>
2023-10-26 12:57:43 +0200

    Fixed mistake 2

--- mysql.sql
+++ mysql.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `emko01_skp_dp_sde_dk`
+-- Database: `LagerProjekt`
 --
 CREATE DATABASE IF NOT EXISTS `LagerProjekt` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 USE `LagerProjekt`;
