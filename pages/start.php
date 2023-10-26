41d5e74b4a0a9620b6d8cd23dc13caf6aab87694
juele <juele@10.130.16.73>
2023-10-26 13:08:31 +0200

    Updated text

--- pages/start.php
+++ pages/start.php
@@ -1,3 +1,3 @@
 <h1>&nbsp;Emils store</h1>
 <p>&nbsp;Buy what you want please.
-<br>&nbsp;We specialize in everything from computers to screens, we know how important lasagna is, too.</p>
+<br>&nbsp;We specialize in everything from computers to screens, we know how important a good customer experience is, too.</p>
